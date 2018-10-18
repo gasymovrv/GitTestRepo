4494c20304e8f3e125c72ca0df5f7f6311bd28e1
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-18 16:43:53 +0400

    - method2

--- src/java/Connection.java
+++ src/java/Connection.java
@@ -1,5 +1,5 @@
 public class Connection {
-    private void connect(String[] args) {
-
+    public String connect() {
+        return "connecting";
     }
 }
--- src/java/Main.java
+++ src/java/Main.java
@@ -1,6 +1,25 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("еще один коммит в новую ветку");
         System.out.println("не надо мне тут бла-бла-бла");
+        Class entityClass = Entity.class;
+        Method[] methods = entityClass.getMethods();
+        Connection connection = new Connection();
+        System.out.println(connection.connect());
+        int i = 0;
+        String s = "string";
+        for (Method m :
+                methods) {
+            try {
+                if (m.getDeclaringClass().equals(entityClass)) {
+                    System.out.println(m.invoke(entityClass.newInstance(),s+i,i++));
+                }
+            } catch (IllegalAccessException | InvocationTargetException | IllegalArgumentException | InstantiationException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
