0e7ba26e5a1582e308677515956ae70df6ce4a8d
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-18 15:35:18 +0400

    methods 1,2,3

--- src/java/Entity.java
+++ src/java/Entity.java
@@ -1,11 +1,11 @@
 public class Entity {
-    public static void method2(String[] args) {
-
+    public static String method1(String[] args) {
+        return "method1";
     }
-    public static void method3(String[] args) {
-
+    public static String method2(String[] args) {
+        return "method2";
     }
-    public static void method4(String[] args) {
-
+    public static String method3(String[] args) {
+        return "method3";
     }
 }
