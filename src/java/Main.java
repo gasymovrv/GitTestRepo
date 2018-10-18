820ce3a41af53deb61683f1d9f7a2bb167d7170a
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-18 16:54:31 +0400

    - method2

--- src/java/Main.java
+++ src/java/Main.java
@@ -3,4 +3,5 @@ public class Main {
         System.out.println("еще один коммит в новую ветку");
         System.out.println("не надо мне тут бла-бла-бла");
     }
+    public static void util(){}
 }
