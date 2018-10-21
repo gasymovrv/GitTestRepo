867037566a64d93a5306dbe4b01646b6c7bf1373
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-21 14:44:17 +0400

    merge compare rebase

--- src/java/Entity.java
+++ src/java/Entity.java
@@ -8,4 +8,7 @@ public class Entity {
     public  String method3(String s, int a) {
         return "method3_s="+s+"_int="+a;
     }
+    public  String method4(String s, int a) {
+        return "merge compare rebase";
+    }
 }
