6f8cddc5b238f510d4294d0f4cc9bb89042ac601
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-24 13:34:38 +0400

    какой-то доп. функционал

--- src/java/Feature1.java
+++ src/java/Feature1.java
@@ -5,4 +5,7 @@ public class Feature1 {
     public void doing2() {
         System.out.println("какой-то доп. функционал №1");
     }
+    public void doing3() {
+        System.out.println("какой-то доп. функционал №1");
+    }
 }
