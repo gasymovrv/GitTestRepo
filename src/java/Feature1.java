8fe1dad351dcb8a21119186be9d87f8d0cf8b6f7
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-24 10:24:28 +0400

    feature1.1

--- src/java/Feature1.java
+++ src/java/Feature1.java
@@ -2,4 +2,7 @@ public class Feature1 {
     public void doing() {
         System.out.println("какой-то функционал №1");
     }
+    public void doing2() {
+        System.out.println("какой-то доп. функционал №1");
+    }
 }
